d34594d341d7f9d06c943e148e2fd72032590975
M-D-Y <muratovd@yandex.ru>
2016-03-03 02:12:48 +0300

    Atomic demo added

---
+++ Java.Util.Concurent/src/AtomicDemo.java
@@ -0,0 +1,31 @@
+
+//Простой пример выполнения атомарных операций
+import java.util.concurrent.atomic.*;
+
+public class AtomicDemo {
+	public static void main(String args[]) {
+		new AtomThread("A");
+		new AtomThread("B");
+		new AtomThread("C");
+	}
+}
+
+class SharedForAtomicDemo {
+	static AtomicInteger ai = new AtomicInteger(0);
+}
+
+// Потоки сполнения, в котором инкрементируется значение счетчика
+class AtomThread implements Runnable {
+	String name;
+
+	AtomThread(String n) {
+		name = n;
+		new Thread(this).start();
+	}
+
+	public void run() {
+		System.out.println("Зanycк потока " + name);
+		for (int i = 1; i <= 3; i++)
+			System.out.println("Пoтoк " + name + " получено:" + SharedForAtomicDemo.ai.getAndSet(i));
+	}
+}
